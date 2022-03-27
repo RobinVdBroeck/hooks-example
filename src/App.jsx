f786edf1a74105f3cabc80db8ca467fcf8765019
Robin Van den Broeck <robin.broeck@gmail.com>
2022-03-27 12:47:10 +0200

    Allow to choose implementation

--- src/App.jsx
+++ src/App.jsx
@@ -1,166 +1,25 @@
-import React from "react";
-import { v4 as uuid } from "uuid";
-import LanguageText  from "./LanguageText";
-import Stats from "./Stats";
-import TodoItem from "./TodoItem";
-import en from "./translations/en.json";
-import nl from "./translations/nl.json";
+import React, { useState, useMemo } from "react";
+import Cnc from "./classes-non-context/App";
 
-class App extends React.Component {
-  static languages = {
-    en,
-    nl,
-  };
+const App = () => {
+    const [implementation, setImplementation] = useState("classes-non-context");
 
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      todos: [],
-      newTodo: {
-        id: uuid(),
-        text: "",
-        completed: false,
-      },
-      selectedLanguage: "en",
-    };
-  }
-
-  render() {
-    return (
-      <>
-        <h1>
-          <LanguageText
-            languages={App.languages}
-            selectedLanguage={this.state.selectedLanguage}
-            translationKey="todo-app"
-          />
-        </h1>
-        <h2>
-          <LanguageText
-            languages={App.languages}
-            selectedLanguage={this.state.selectedLanguage}
-            translationKey="language-selection"
-          />
-        </h2>
-        <div>
-          <button onClick={this.setLanguage("en")}>
-            <LanguageText
-              languages={App.languages}
-              selectedLanguage={this.state.selectedLanguage}
-              translationKey="english"
-            />
-          </button>
-          <button onClick={this.setLanguage("nl")}>
-            <LanguageText
-              languages={App.languages}
-              selectedLanguage={this.state.selectedLanguage}
-              translationKey="dutch"
-            />
-          </button>
-        </div>
-        <h2>
-          <LanguageText
-            languages={App.languages}
-            selectedLanguage={this.state.selectedLanguage}
-            translationKey="todo-list"
-          />
-        </h2>
-        <ul>
-          {this.state.todos.map((todo) => (
-            <TodoItem
-              key={todo.id}
-              text={todo.text}
-              selectedLanguage={this.state.selectedLanguage}
-              languages={App.languages}
-              completed={todo.completed}
-              onCompleteChange={this.onCompleteChange(todo)}
-            />
-          ))}
-          {this.state.newTodo.text !== "" && (
-            <TodoItem
-              text={this.state.newTodo.text}
-              selectedLanguage={this.state.selectedLanguage}
-              languages={App.languages}
-              draft
-            />
-          )}
-        </ul>
-        <input
-          placeholder="New todo item"
-          value={this.state.newTodo.text}
-          onChange={this.onChange}
-        />
-        <button type="button" onClick={this.addTodo}>
-          <LanguageText
-            languages={App.languages}
-            selectedLanguage="en"
-            translationKey="add"
-          />
-        </button>
-        <Stats
-          todos={this.state.todos}
-          languages={App.languages}
-          selectedLanguage={this.state.selectedLanguage}
-        />
-      </>
-    );
-  }
-
-  setLanguage = (lang) => () => {
-    this.setState((state) => ({
-      ...state,
-      selectedLanguage: lang,
-    }));
-  };
-
-  onChange = ($e) => {
-    this.setState((state) => ({
-      ...state,
-      newTodo: {
-        ...state.newTodo,
-        text: $e.target.value,
-      },
-    }));
-  };
-
-  addTodo = () => {
-    if (this.state.newTodo.text.trim() === "") {
-      return;
-    }
-
-    this.setState((state) => ({
-      ...state,
-      todos: [
-        ...state.todos,
-        {
-          ...state.newTodo,
-          text: state.newTodo.text.trim(),
-        },
-      ],
-      newTodo: {
-        id: uuid(),
-        text: "",
-      },
-    }));
-  };
-
-  onCompleteChange = (item) => {
-    return (completed) => {
-      this.setState((state) => {
-        let todos = [...state.todos];
-        const idx = todos.findIndex((todo) => todo === item);
-        if (idx == -1) {
-          throw new Error();
+    const app = useMemo(() => {
+        switch(implementation) {
+            case "classes-non-context":
+                return <Cnc />;
+            default:
+                return <div>Unknown implementation</div>
         }
-        todos[idx] = { ...todos[idx], completed };
-        return {
-          ...state,
-          todos,
-        };
-      });
-    };
-  };
-}
+    }, [implementation]);
+
+    return (
+        <div>
+            { app }
+            <h2>Implementation</h2>
+            <button onClick={() => setImplementation("classes-non-context")}>Classes non context</button>
+        </div>
+    )
+};
 
 export default App;
